e854f63540bd49756fc98ce35978a1ccaedf0fd3
TAnetwork <39844863+TAnetwork@users.noreply.github.com>
2023-11-01 15:25:00 +0200

    Update 47dbfd8a2e201257d781622188836d0c.js

--- js/47dbfd8a2e201257d781622188836d0c.js
+++ js/47dbfd8a2e201257d781622188836d0c.js
@@ -1,11 +1,20 @@
-var PINEAPPLE = PINEAPPLE || (function(){
-	var _args = {};
-	return {
-		init : function(Args) {
-			_args = Args;
-		},
-		plum : function() {
-			url = parent.document.URL; if (window.innerWidth > 985) { document.write('<div id="970x250"><div id="main" style=" width: 985px; height: 267px;"><iframe id="adframe" src="https://www.topad.net/a/970x250/?r=' + _args[0] + '" style="border-width: 0; width: 985px; height: 267px;"></iframe><iframe id="if2" src="https://www.topad.net/vi/?r=' + _args[0] + '" style="display: none;"></iframe></div></div>')}; document.getElementById("if2").onload = function() {myFunction()}; function myFunction() { var myobj = document.getElementById("if2"); myobj.remove(); } document.getElementById('ta-47dbfd8a2e201257d781622188836d0c').appendChild(document.getElementById('970x250'));
-		}
-	};
-}());
+var boom = document.querySelectorAll('.topad-ad970x250');
+document.getElementsByClassName("topad").offsetWidth = 985;
+document.getElementsByClassName("topad").offsetHeight = 267px;
+if (window.innerWidth > 1024) {
+    const nonDisplayedIframe = document.createElement('iframe');
+    nonDisplayedIframe.style.display = 'none';
+    nonDisplayedIframe.setAttribute('src', "https://www.topad.net/vi/?r=" + psec);
+    document.body.appendChild(nonDisplayedIframe);
+    nonDisplayedIframe.addEventListener('load', () => {
+        nonDisplayedIframe.remove();
+        [].forEach.call(boom, function(bom){
+        const adIframe = document.createElement('iframe');
+        adIframe.setAttribute('src', "https://www.topad.net/a/970x250/?r=" + psec);
+        adIframe.style.border = 'none';
+        adIframe.width = 985;
+        adIframe.height = 267px;
+        bom.appendChild(adIframe);
+    });
+  });
+}
